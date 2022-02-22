dca3fb4ace02e75ac1c55558d3fc5800e62111ca
Vin.AM <75691501+NickName-AM@users.noreply.github.com>
2022-02-22 22:02:50 +0545

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,171 @@
+import random
+import os, sys
+from time import sleep
+
+hangman = [['''
+  O
+''',
+'''
+  .
+  O
+''',
+'''
+  .
+  .
+  O
+''',
+'''
+  .
+  .
+  O
+_____
+''',
+'''
+  .
+  .
+  .  
+_____
+'''],
+'''
+  O
+  |
+''',
+'''
+  O
+ /|
+''',
+r'''
+  O
+ /|\
+''',
+r'''
+  O
+ /|\
+  |
+''',
+r'''
+  O
+ /|\
+  |
+ /
+''',
+r'''
+  O
+ /|\
+  |
+ / \
+'''
+]
+
+def OS_CHECK():
+  accepted_OS = ["win32", "linux"]
+  if sys.platform not in accepted_OS:
+    input("Only for Windows and linux.\n Press Enter to exit.")
+    exit()
+
+def clear():
+  if sys.platform == "win32":
+    os.system("CLS")
+  elif sys.platform == "linux":
+    os.system("clear")
+  #elif sys.platform == "OS_NAME":             ## Go to python.org to find the OS_NAME for your OS 
+  #  os.system(" ADD YOUR COMMAND HERE. ")
+  
+def to_print():
+  # Print the doll to be hanged
+  print(hangman[man])
+
+  # Print the "_ _ _ ..."
+  print(' '.join(letters))
+  print("\nLetters typed so far: {0}".format(''.join(already_typed)))
+
+# Index of multiple characters in a string
+def indices(word, letter):
+    char_list = list(word.lower())
+    indexs = list()
+    for i in char_list:
+        if i == letter.lower():
+            indexs.append(char_list.index(i))
+            char_list[char_list.index(i)] = "00"
+    return indexs
+
+def lost_animation():
+  a = 0
+  while a <= 4:
+    clear()
+    print(hangman[0][a])
+    a+=1
+    sleep(0.4)
+  print("Correct word: {0}".format(random_word))
+  sleep(2)
+  exit()
+
+# Main
+def final(word):
+    global man
+    man = 6           # For 'hangman'
+    while True:
+        clear()
+
+        # If all chances are lost.
+        if man == 0:
+          lost_animation()
+
+        to_print()
+
+        # Win condition
+        if ''.join(letters) == word:
+          print("You win!")
+          sleep(2)
+          exit()
+
+        letter_input = ""
+        
+        # If the input has more or less than 1 characters or if the given letter has already been typed before, loop
+        while (len(letter_input) != 1) or (letter_input in already_typed):
+          letter_input = input("\nA letter : ").lower()
+          clear()
+          to_print()
+
+        already_typed.append(letter_input)
+        already_typed.append(" ")
+        # There might be multiple spaces that might have same characters
+        indices_ = indices(word, letter_input)
+
+        if indices_ != []:
+            # Replace the "_" with the given letter if true
+            for i in indices_:
+              if i == 0:
+                letters[i] = letter_input.upper()
+              else:
+                letters[i] = letter_input
+        else:
+            # Chop a part of the body
+            man -= 1
+
+
+OS_CHECK()
+
+
+# Add more if you want
+word_list = ["volley", "table tennis", "yeet", "heat", "beach", "watch", "sakura flower", "sakura tree", "saber", "kujou sara", "ganyu", "zhongli", "xinqiu", "morax", "azdaha", "pogchamp", "rickroll", "never gonna give you up", "never gonna let you down", "mouse", "wind", "shadow", "yanfei", "football", "tartaglia", "underworld", "truth", "word", "keyboard", "laptop", "curtain", "mecha", "shogun"]
+
+# Choose a random word from word_list
+random_word = random.choice(word_list).lower().capitalize()
+
+
+# length of the random word
+word_len = len(random_word)
+
+letters = ["_"] * word_len
+
+if " " in random_word:
+  space_index = indices(random_word, " ")
+  for i in space_index:
+    letters[i] = " "
+
+# To check if the input letter has been entered before
+already_typed = list()
+
+# Call the main function
+final(random_word)
